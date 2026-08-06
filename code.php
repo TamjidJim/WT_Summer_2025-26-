853dd797913e84128cfcec38ae0de10d1a2e3f65
TamjidJim <jim_infinity@Zims-Mac.local>
2026-08-06 10:35:13 +0600

    First PHP code

---
+++ code.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h1>This is our 1st PHP class</h1>
+
+<?php
+$favcolor = "red";
+
+switch ($favcolor) {
+    case "red":
+        echo "Your favorite color is red!";
+        break;
+
+    case "blue":
+        echo "Your favorite color is blue!";
+        break;
+
+    case "green":
+        echo "Your favorite color is green!";
+        break;
+
+    default:
+        echo "Your favorite color is neither red, blue, nor green!";
+}
+?>
+
+</body>
+
+</html>
---
+++ in.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<h1>This is our 1st php class </h1>
+
+ 
+
+<?php
+
+echo"Hello World <br> ";
+
+ 
+
+
+
+?>
+
+ 
+
+</body>
+
+</html>
+
